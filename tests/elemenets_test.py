98324d40503d2f9a389d0d85a43ef72100ab4d7a
igorkorobchenko <korobchenko.igor@gmail.com>
2025-02-06 15:22:23 -0800

    Added test: test_web_table_search_person

--- tests/elemenets_test.py
+++ tests/elemenets_test.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 from pages.base_page import BasePage
@@ -54,3 +55,12 @@ class TestWebTable:
         new_person = web_table_page.add_new_person()
         table_result = web_table_page.check_new_added_person()
         assert  new_person in table_result
+
+    def test_web_table_search_person(self, driver):
+        web_table_page = WebTablePage(driver, 'http://demoqa.com/webtables')
+        web_table_page.open()
+        key_word = web_table_page.add_new_person()[random.randint(0,5)]
+        web_table_page.search_some_person(key_word)
+        table_result = web_table_page.check_search_person()
+        assert key_word in table_result, "the person wasn't found in the table"
+
