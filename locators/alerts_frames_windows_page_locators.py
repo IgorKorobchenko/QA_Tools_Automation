6d80d19eb8bab9ffc2b097ca63a96874e7975964
igorkorobchenko <korobchenko.igor@gmail.com>
2025-05-06 16:30:11 -0700

    Added: Test_Alert_Page Added tests: -test_see_alert, -test_see_alert_after_five_sec, -test_check_confirm_alert, -test_check_prompt_alert

--- locators/alerts_frames_windows_page_locators.py
+++ locators/alerts_frames_windows_page_locators.py
@@ -5,3 +5,11 @@ class BrowserWindowsPageLocators:
     NEW_TAB_BUTTON = (By.XPATH, '//*[@id="tabButton"]')
     TITLE_NEW = (By.XPATH, '//*[@id="sampleHeading"]')
     NEW_WINDOW_BUTTON = (By.XPATH, '//*[@id="windowButton"]')
+
+class AlertsPageLocators:
+    SEE_ALERT_BUTTON = (By.XPATH, '//*[@id="alertButton"]')
+    APPEAR_ALERT_AFTER_5_SEC_BUTTON = (By.XPATH, '//*[@id="timerAlertButton"]')
+    CONFIRM_BOX_ALERT_BUTTON = (By.XPATH, '//*[@id="confirmButton"]')
+    PROMPT_BOX_ALERT_BUTTON = (By.XPATH, '//*[@id="promtButton"]')
+    CONFIRM_RESULT = (By.XPATH, '//*[@id="confirmResult"]')
+    PROMPT_RESULT = (By.XPATH, '//*[@id="promptResult"]')
