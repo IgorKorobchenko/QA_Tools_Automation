d978e339b59b612555a8f9f094d17d3f72c2a45a
igorkorobchenko <korobchenko.igor@gmail.com>
2025-05-30 15:37:19 -0700

    Added: AutoCompletePage.py, Added tests: test_fill_multi_autocomplete, test_remove_value_from_multi, test_fill_single_autocomplete

--- locators/widgets_page_locators.py
+++ locators/widgets_page_locators.py
@@ -8,3 +8,10 @@ class AccordianPageLocators:
     CONTENT_SECOND= (By.XPATH, '//*[@id="section2Content"]')
     SECTION_THIRD = (By.XPATH, '//*[@id="section3Heading"]')
     CONTENT_THIRD = (By.XPATH, '//*[@id="section3Content"]')
+
+class AutoCompletePageLocators:
+    MULTI_INPUT = (By.XPATH, '//*[@id= "autoCompleteMultipleInput"]')
+    MULTI_VALUE = (By.XPATH, '//*[@class="css-1rhbuit-multiValue auto-complete__multi-value"]')
+    MULTI_VALUE_REMOVE = (By.XPATH, '//*[@class="css-xb97g8 auto-complete__multi-value__remove"]')
+    SINGLE_VALUE = (By.XPATH, '//*[@class= "auto-complete__single-value css-1uccc91-singleValue"]')
+    SINGLE_INPUT = (By.XPATH, '//*[@id= "autoCompleteSingleInput"]')
